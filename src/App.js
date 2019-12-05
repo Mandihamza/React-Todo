dcc3111bebedfaf907b6332d318074f921e623db
mandihamza <mandihamza@gmail.com>
2019-12-04 19:29:36 -0700

    Adds toggle state handler

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
 import { todos } from "./data"; 
+import "./components/Todo.css"
 
 import TodoList from './components/TodoList';
 
 class App extends Component {
+  // you will need a place to store your state in this component.
   constructor() {
     super();
     this.state = {
@@ -12,12 +14,27 @@ class App extends Component {
     };
   }
 
+  toggleTodo = itemId => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (itemId === todo.id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo;
+      })
+    });
+  };
+
   render() {
     return (
       <div>
         <h2>Keep your kitty organized with Catodos. Get started right meow!</h2>
         <TodoList 
         todos={this.state.todos}
+        toggleTodo={this.toggleTodo}
         />
       </div>
     );
