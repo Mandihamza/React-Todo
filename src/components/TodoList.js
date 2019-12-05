a4a552ad36e3756feef780ba388fbc8eb162e51e
mandihamza <mandihamza@gmail.com>
2019-12-04 17:58:39 -0700

    Adds basic todo list component [WIP]

---
+++ src/components/TodoList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Todo from "./Todo";
+
+const TodoList = props => {
+  return (
+    <div>
+      {props.todos.map(todo => {
+        return <Todo todo={todo} key={todo.id} />;
+      })}
+    </div>
+  );
+};
+
+export default TodoList;
