0e5fe87b23b4ebebde22666f1fd1cd5a69e2b6c9
mandihamza <mandihamza@gmail.com>
2019-12-04 17:31:40 -0700

    Adds basic Todo UI component

---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Todo = props => {
+  return (
+    <div
+      className={`todo${props.todo.completed ? " completed" : ""}`}>
+      {props.todo.task}
+    </div>
+  );
+};
+
+export default Todo;
