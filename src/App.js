417c1ec1e3bb9ad8f9cc09ff4b05f971ece67c2a
mandihamza <mandihamza@gmail.com>
2019-12-04 18:25:30 -0700

    Adds todo list component and sets initial state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+import { todos } from "./data"; 
+
+import TodoList from './components/TodoList';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todos
+    };
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Keep your kitty organized with Catodos. Get started right meow!</h2>
+        <TodoList 
+        todos={this.state.todos}
+        />
       </div>
     );
   }
